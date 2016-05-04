f43394777e0a1e2e5f5aee7a74d0f674c5712e45
Davide Mottin <davide.mottin@gmail.cm>
2016-05-04 11:45:09 +0200

    Added a class for triples

---
+++ src/main/java/eu/unitn/disi/db/mutilities/Triple.java
@@ -0,0 +1,110 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Davide Mottin <mottin@disi.unitn.eu>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package eu.unitn.disi.db.mutilities;
+
+import java.util.Objects;
+
+/**
+ * A simple class representing a triple of objects
+ * 
+ * @author Davide Mottin <mottin@disi.unitn.eu>
+ * @param <T1>
+ * @param <T2>
+ * @param <T3>
+ */
+public class Triple<T1,T2,T3> {
+    private T1 first; 
+    private T2 second; 
+    private T3 third;
+
+    public Triple() {
+        
+    }
+
+    public Triple(T1 first, T2 second, T3 third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+    
+    public T1 getFirst() {
+        return first;
+    }
+
+    public void setFirst(T1 first) {
+        this.first = first;
+    }
+
+    public T2 getSecond() {
+        return second;
+    }
+
+    public void setSecond(T2 second) {
+        this.second = second;
+    }
+
+    public T3 getThird() {
+        return third;
+    }
+
+    public void setThird(T3 third) {
+        this.third = third;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Triple<?, ?, ?> other = (Triple<?, ?, ?>) obj;
+        if (!Objects.equals(this.first, other.first)) {
+            return false;
+        }
+        if (!Objects.equals(this.second, other.second)) {
+            return false;
+        }
+        if (!Objects.equals(this.third, other.third)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + first + ", " + second + ", " + third + '>';
+    }
+}
